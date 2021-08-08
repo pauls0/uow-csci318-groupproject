2a864ab4c3add6d6b4d8444d28b7684d044933ad
pauls0 <pauls0@users.noreply.github.com>
2021-08-08 17:54:22 +1000

    build(release): Initialize Spring Boot Project

---
+++ customerSystem/src/test/java/com/csci318teamone/customerSystem/CustomerSystemApplicationTests.java
@@ -0,0 +1,11 @@
+package com.csci318teamone.customerSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerSystemApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
