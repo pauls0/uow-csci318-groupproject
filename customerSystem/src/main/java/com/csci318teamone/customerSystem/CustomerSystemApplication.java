26af16b10dac1f002f7cf31f54eca2ffe0b39186
pauls0 <pauls0@users.noreply.github.com>
2021-08-12 03:17:35 +1000

    style(customer service): Format with Prettier

--- customerSystem/src/main/java/com/csci318teamone/customerSystem/CustomerSystemApplication.java
+++ customerSystem/src/main/java/com/csci318teamone/customerSystem/CustomerSystemApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CustomerSystemApplication {
+
   public static void main(String[] args) {
     SpringApplication.run(CustomerSystemApplication.class, args);
   }
