f49e650296849d76bddd77ee0403eb7689944d56
pauls0 <pauls0@users.noreply.github.com>
2021-08-12 03:18:22 +1000

    style(customer service): Format with Prettier

--- customerSystem/src/main/java/com/csci318teamone/customerSystem/controllers/CustomerController.java
+++ customerSystem/src/main/java/com/csci318teamone/customerSystem/controllers/CustomerController.java
@@ -2,43 +2,46 @@ package com.csci318teamone.customerSystem.controllers;
 
 import com.csci318teamone.customerSystem.entities.Customer;
 import com.csci318teamone.customerSystem.services.CustomerService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/customers")
 public class CustomerController {
-    private CustomerService customerService;
 
-    @Autowired
-    public void setCustomerRepository(CustomerService customerService) {
-        this.customerService = customerService;
-    }
+  private CustomerService customerService;
 
-    @GetMapping
-    List<Customer> getCustomers() {
-        return customerService.getCustomers();
-    }
+  @Autowired
+  public void setCustomerRepository(CustomerService customerService) {
+    this.customerService = customerService;
+  }
 
-    @PostMapping
-    Customer createCustomer(@RequestBody Customer newCustomer) {
-        return customerService.createCustomer(newCustomer);
-    }
+  @GetMapping
+  List<Customer> getCustomers() {
+    return customerService.getCustomers();
+  }
 
-    @GetMapping("{id}")
-    Customer getCustomerById(@PathVariable Long id) {
-        return customerService.getCustomerById(id);
-    }
+  @PostMapping
+  Customer createCustomer(@RequestBody Customer newCustomer) {
+    return customerService.createCustomer(newCustomer);
+  }
 
-    @PutMapping("{id}")
-    Customer updateCustomerById(@PathVariable Long id, @RequestBody Customer newCustomer) {
-        return customerService.updateCustomerById(id, newCustomer);
-    }
+  @GetMapping("{id}")
+  Customer getCustomerById(@PathVariable Long id) {
+    return customerService.getCustomerById(id);
+  }
 
-    @DeleteMapping("{id}")
-    void deleteCustomerById(@PathVariable Long id) {
-        customerService.deleteCustomerById(id);
-    }
+  @PutMapping("{id}")
+  Customer updateCustomerById(
+    @PathVariable Long id,
+    @RequestBody Customer newCustomer
+  ) {
+    return customerService.updateCustomerById(id, newCustomer);
+  }
+
+  @DeleteMapping("{id}")
+  void deleteCustomerById(@PathVariable Long id) {
+    customerService.deleteCustomerById(id);
+  }
 }
--- customerSystem/src/main/java/com/csci318teamone/customerSystem/services/CustomerService.java
+++ customerSystem/src/main/java/com/csci318teamone/customerSystem/services/CustomerService.java
@@ -1,17 +1,16 @@
 package com.csci318teamone.customerSystem.services;
 
 import com.csci318teamone.customerSystem.entities.Customer;
-
 import java.util.List;
 
 public interface CustomerService {
-    List<Customer> getCustomers();
+  List<Customer> getCustomers();
 
-    Customer getCustomerById(Long id);
+  Customer getCustomerById(Long id);
 
-    Customer createCustomer(Customer Customer);
+  Customer createCustomer(Customer Customer);
 
-    Customer updateCustomerById(Long id, Customer newCustomer);
+  Customer updateCustomerById(Long id, Customer newCustomer);
 
-    void deleteCustomerById(Long id);
+  void deleteCustomerById(Long id);
 }
