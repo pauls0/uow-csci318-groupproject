4f86569cf92f13da9b13aa6e9fe38bfa8f855773
MichaelKilbane <me@michaelkilbane.com>
2021-08-12 03:17:30 +1000

    feat: Implement Customer Service

---
+++ customerSystem/src/main/java/com/csci318teamone/customerSystem/entities/Customer.java
@@ -0,0 +1,76 @@
+package com.csci318teamone.customerSystem.entities;
+
+import javax.persistence.*;
+
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+
+    private String companyName;
+    private String address;
+    private String country;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn
+    private Contact contact;
+
+    protected Customer() {}
+
+    public Customer(String companyName, String address, String country, Contact contact) {
+        this.companyName = companyName;
+        this.address = address;
+        this.country = country;
+        this.contact = contact;
+
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Customer[id=%d, companyName='%s', address='%s', country='%s', contact='%s']",
+                id, companyName, address, country, contact);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public Contact getContact() {
+        return contact;
+    }
+
+    public void setContact(Contact contact) {
+        this.contact = contact;
+    }
+}
