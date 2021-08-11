1dc904b208984b699055d32c182b0044b3d2eddb
pauls0 <pauls0@users.noreply.github.com>
2021-08-12 03:18:22 +1000

    style(customer service): Format with Prettier

--- customerSystem/src/main/java/com/csci318teamone/customerSystem/configuration/LoadDatabase.java
+++ customerSystem/src/main/java/com/csci318teamone/customerSystem/configuration/LoadDatabase.java
@@ -11,40 +11,35 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 class LoadDatabase {
-    private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
 
-    @Bean
-    CommandLineRunner initDatabase(CustomerRepository customerRepository) {
-        return args -> {
-            log.info("Preloading " + customerRepository.save(
-                    new Customer(
-                        "Apple",
-                        "1 Apple Park Way, Cupertino, CA 95014",
-                        "USA",
-                        new Contact(
-                            "Tim Apple",
-                            "123456",
-                            "tim@apple.com",
-                            "CEO"
-                        )
-                    )
-                )
-            );
+  private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
 
-            log.info("Preloading " + customerRepository.save(
-                    new Customer(
-                        "SpaceX",
-                        "42 Galactic Drive, San Francisco, CA 97468",
-                        "USA",
-                        new Contact(
-                            "Elon Musk",
-                            "69420",
-                            "elon@spacex.com",
-                            "CEO"
-                        )
-                    )
-                )
-            );
-        };
-    }
+  @Bean
+  CommandLineRunner initDatabase(CustomerRepository customerRepository) {
+    return args -> {
+      log.info(
+        "Preloading " +
+        customerRepository.save(
+          new Customer(
+            "Apple",
+            "1 Apple Park Way, Cupertino, CA 95014",
+            "USA",
+            new Contact("Tim Apple", "123456", "tim@apple.com", "CEO")
+          )
+        )
+      );
+
+      log.info(
+        "Preloading " +
+        customerRepository.save(
+          new Customer(
+            "SpaceX",
+            "42 Galactic Drive, San Francisco, CA 97468",
+            "USA",
+            new Contact("Elon Musk", "69420", "elon@spacex.com", "CEO")
+          )
+        )
+      );
+    };
+  }
 }
