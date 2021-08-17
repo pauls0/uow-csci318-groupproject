431fed44aafa8e413b609627d0374f0493673264
pauls0 <pauls0@users.noreply.github.com>
2021-08-17 15:48:32 +1000

    Format with Prettier

--- customerSystem/src/main/java/com/csci318teamone/customerSystem/entities/Customer.java
+++ customerSystem/src/main/java/com/csci318teamone/customerSystem/entities/Customer.java
@@ -5,72 +5,81 @@ import javax.persistence.*;
 @Entity
 public class Customer {
 
-    @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
 
-    private String companyName;
-    private String address;
-    private String country;
+  private String companyName;
+  private String address;
+  private String country;
 
-    @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn
-    private Contact contact;
+  @OneToOne(cascade = CascadeType.ALL)
+  @JoinColumn
+  private Contact contact;
 
-    protected Customer() {}
+  protected Customer() {}
 
-    public Customer(String companyName, String address, String country, Contact contact) {
-        this.companyName = companyName;
-        this.address = address;
-        this.country = country;
-        this.contact = contact;
+  public Customer(
+    String companyName,
+    String address,
+    String country,
+    Contact contact
+  ) {
+    this.companyName = companyName;
+    this.address = address;
+    this.country = country;
+    this.contact = contact;
+  }
 
-    }
+  @Override
+  public String toString() {
+    return String.format(
+      "Customer[id=%d, companyName='%s', address='%s', country='%s', contact='%s']",
+      id,
+      companyName,
+      address,
+      country,
+      contact
+    );
+  }
 
-    @Override
-    public String toString() {
-        return String.format(
-                "Customer[id=%d, companyName='%s', address='%s', country='%s', contact='%s']",
-                id, companyName, address, country, contact);
-    }
+  public Long getId() {
+    return id;
+  }
 
-    public Long getId() {
-        return id;
-    }
+  public void setId(Long id) {
+    this.id = id;
+  }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+  public String getCompanyName() {
+    return companyName;
+  }
 
-    public String getCompanyName() {
-        return companyName;
-    }
+  public void setCompanyName(String companyName) {
+    this.companyName = companyName;
+  }
 
-    public void setCompanyName(String companyName) {
-        this.companyName = companyName;
-    }
+  public String getAddress() {
+    return address;
+  }
 
-    public String getAddress() {
-        return address;
-    }
+  public void setAddress(String address) {
+    this.address = address;
+  }
 
-    public void setAddress(String address) {
-        this.address = address;
-    }
+  public String getCountry() {
+    return country;
+  }
 
-    public String getCountry() {
-        return country;
-    }
+  public void setCountry(String country) {
+    this.country = country;
+  }
 
-    public void setCountry(String country) {
-        this.country = country;
-    }
+  public Contact getContact() {
+    return contact;
+  }
 
-    public Contact getContact() {
-        return contact;
-    }
-
-    public void setContact(Contact contact) {
-        this.contact = contact;
-    }
+  public void setContact(Contact contact) {
+    this.contact = contact;
+  }
 }
