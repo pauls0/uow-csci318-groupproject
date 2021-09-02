a167571fbd08ec7e5ceea616ae00e7c7d844fae4
pauls0 <pauls0@users.noreply.github.com>
2021-09-02 11:35:45 +1000

    feat: Implement Product Service

---
+++ productSystem/src/test/java/com/csci318teamone/productSystem/ProductSystemApplicationTests.java
@@ -0,0 +1,11 @@
+package com.csci318teamone.productSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductSystemApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
