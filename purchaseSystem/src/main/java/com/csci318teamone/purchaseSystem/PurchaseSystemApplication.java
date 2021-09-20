cfcb40b3e19cedba1e764451b9f43d03d3c024bf
pauls0 <pauls0@users.noreply.github.com>
2021-09-20 22:28:37 +1000

    feat(Application): bean for WebClientBuilder

--- purchaseSystem/src/main/java/com/csci318teamone/purchaseSystem/PurchaseSystemApplication.java
+++ purchaseSystem/src/main/java/com/csci318teamone/purchaseSystem/PurchaseSystemApplication.java
@@ -2,11 +2,18 @@ package com.csci318teamone.purchaseSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class PurchaseSystemApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(PurchaseSystemApplication.class, args);
+  }
+
+  @Bean
+  public WebClient.Builder getWebClientBuilder() {
+    return WebClient.builder();
   }
 }
