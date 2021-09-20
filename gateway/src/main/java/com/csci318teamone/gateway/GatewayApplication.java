353c240a5261b4ed56d9cfd5ed60632bfb9f4075
pauls0 <pauls0@users.noreply.github.com>
2021-09-20 22:40:26 +1000

    feat(gateway): define routes

--- gateway/src/main/java/com/csci318teamone/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/csci318teamone/gateway/GatewayApplication.java
@@ -2,11 +2,24 @@ package com.csci318teamone.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(GatewayApplication.class, args);
+  }
+
+  @Bean
+  public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+    return builder
+      .routes()
+      .route(p -> p.path("/customers").uri("http://localhost:8081/customers"))
+      .route(p -> p.path("/products").uri("http://localhost:8082/products"))
+      .route(p -> p.path("/purchases").uri("http://localhost:8083/purchases"))
+      .build();
   }
 }
